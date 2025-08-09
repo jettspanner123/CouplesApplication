e2943ad94a14964f44682912420211d139b0e73f
jettspanner123 <uddeshyasingh12bsci@gmail.com>
2025-08-09 10:33:52 +0530

    Init: Initial Project Setups

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import "./global.css";
+
+export default function App() {
+  return (
+    <View>
+      <Text className={"bg-blue-300"}>Hello, World!</Text>
+    </View>
+  );
+}
